0b76e3c036e5ca3a119cd2aacc42da8575448892
vtaipale <valtter.taipale@gmail.com>
2016-03-16 14:48:52 +0200

    Round now ready
    Round consists of 5 Rooms like BG
    No collision maps or other details yet

--- www/assets/maps/rounds/round1.js
+++ www/assets/maps/rounds/round1.js
@@ -1,5 +1,9 @@
 'use strict';
 
-var round1 = [{"roomBg": "castlebasic.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "east", "moveSpeed": "normal"},
-							{"roomBg": "castlebasic.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "south", "moveSpeed": "normal"},
-              {"roomBg": "castlebasic.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "null", "moveSpeed": "stop"}];
+var round1 = [
+	{"roomBg": "DemoMap1.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "east", "moveSpeed": "normal"},
+	{"roomBg": "DemoMap2.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "south", "moveSpeed": "normal"},
+	{"roomBg": "DemoMap3.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "south", "moveSpeed": "normal"}
+	{"roomBg": "DemoMap4.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "east", "moveSpeed": "normal"}
+	{"roomBg": "DemoMap5.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "null", "moveSpeed": "stop"}
+	];
