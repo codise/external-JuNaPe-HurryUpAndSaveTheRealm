9e79d783eacb888bb0e44cfac45a690e4fe7dcf3
vtaipale <valtter.taipale@gmail.com>
2016-03-22 00:34:24 +0200

    DemoMap 2 + Other Decorations
    Lots of new tiles and few decorations + powerups
    Demomap 2 "Stairs to Tower"

--- www/assets/maps/rounds/round1.js
+++ www/assets/maps/rounds/round1.js
@@ -1,9 +1,11 @@
 'use strict';
 
-var round1 = [
-	{"roomBg": "DemoMap1.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "east", "moveSpeed": "normal"},
-	{"roomBg": "DemoMap2.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "south", "moveSpeed": "normal"},
-	{"roomBg": "DemoMap3.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "south", "moveSpeed": "normal"},
-	{"roomBg": "DemoMap4.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "east", "moveSpeed": "normal"},
-	{"roomBg": "DemoMap5.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "null", "moveSpeed": "stop"}
+
+var round1 = [	//The Second Demo Map aka Stairs to Tower
+	{"roomBg": "Demo2Map-1_Entrance.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "east", "moveSpeed": "normal"},
+	{"roomBg": "Demo2Map-2_DownStairs.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "south", "moveSpeed": "normal"},
+	{"roomBg": "Demo2Map-3_Midstairs.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "south", "moveSpeed": "normal"},
+	{"roomBg": "Demo2Map-4_StairsUp.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "east", "moveSpeed": "normal"},
+	{"roomBg": "Demo2Map-5_Finale.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "null", "moveSpeed": "stop"}
 	];
+
---
+++ www/assets/maps/rounds/round2.js
@@ -0,0 +1,10 @@
+'use strict';
+
+
+var round2 = [	//The First Demo Map
+	{"roomBg": "DemoMap1.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "east", "moveSpeed": "normal"},
+	{"roomBg": "DemoMap2.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "south", "moveSpeed": "normal"},
+	{"roomBg": "DemoMap3.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "south", "moveSpeed": "normal"},
+	{"roomBg": "DemoMap4.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "east", "moveSpeed": "normal"},
+	{"roomBg": "DemoMap5.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "null", "moveSpeed": "stop"}
+	];
