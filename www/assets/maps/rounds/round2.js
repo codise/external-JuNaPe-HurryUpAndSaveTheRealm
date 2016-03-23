1e438c084ee871dc70873c8ef9e316ecd3ae8cfc
Aozi MbAir <niko.novitsky@gmail.com>
2016-03-23 14:43:35 +0200

    Fixed the demo round

--- www/assets/maps/rounds/round2.js
+++ www/assets/maps/rounds/round2.js
@@ -1,10 +1,11 @@
 'use strict';
 
 
-var round2 = [	//The First Demo Map
-	{"roomBg": "DemoMap1.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "east", "moveSpeed": "normal"},
-	{"roomBg": "DemoMap2.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "south", "moveSpeed": "normal"},
-	{"roomBg": "DemoMap3.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "south", "moveSpeed": "normal"},
-	{"roomBg": "DemoMap4.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "east", "moveSpeed": "normal"},
-	{"roomBg": "DemoMap5.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "null", "moveSpeed": "stop"}
+var round2 = [	//The Second Demo Map aka Stairs to Tower
+	{"roomBg": "Demo2Map-1_Entrance.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "east", "moveSpeed": "normal"},
+	{"roomBg": "Demo2Map-2_DownStairs.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "north", "moveSpeed": "normal"},
+	{"roomBg": "Demo2Map-3_Midstairs.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "north", "moveSpeed": "normal"},
+	{"roomBg": "Demo2Map-4_StairsUp.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "east", "moveSpeed": "normal"},
+	{"roomBg": "Demo2Map-5_Finale.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "null", "moveSpeed": "stop"}
 	];
+
