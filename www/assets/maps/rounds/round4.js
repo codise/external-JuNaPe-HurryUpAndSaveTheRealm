8d7cff52c632d6063122dedafaa76947a55ce27d
vtaipale <valtter.taipale@gmail.com>
2016-04-06 15:33:10 +0300

    round4 fixed and added to screen

--- www/assets/maps/rounds/round4.js
+++ www/assets/maps/rounds/round4.js
@@ -5,6 +5,6 @@ var round3 = [	//Walk around the wall grassland
 	{"roomBg": "Demo4Map-1.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "south", "moveSpeed": "normal"},
 	{"roomBg": "Demo4Map-2.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "east", "moveSpeed": "normal"},
 	{"roomBg": "Demo4Map-3.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "north", "moveSpeed": "normal"},
-	{"roomBg": "Demo4Map-4.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "null", "moveSpeed": "normal"},
+	{"roomBg": "Demo4Map-4.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "null", "moveSpeed": "stop"},
 	];
 
