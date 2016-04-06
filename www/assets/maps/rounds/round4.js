a7a148a989b83abc72a00184d1b481f4d3331655
Maerus <maerus.sbkv@gmail.com>
2016-04-06 15:41:53 +0300

    fix master

--- www/assets/maps/rounds/round4.js
+++ www/assets/maps/rounds/round4.js
@@ -2,9 +2,9 @@
 
 
 var round4 = [	//Walk around the wall grassland
-	{"roomBg": "Demo4Map-1.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "south", "moveSpeed": "normal"},
-	{"roomBg": "Demo4Map-2.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "east", "moveSpeed": "normal"},
-	{"roomBg": "Demo4Map-3.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "north", "moveSpeed": "normal"},
-	{"roomBg": "Demo4Map-4.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "null", "moveSpeed": "stop"},
+	{"roomBg": "Demo4Map_1.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "south", "moveSpeed": "normal"},
+	{"roomBg": "Demo4Map_2.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "east", "moveSpeed": "normal"},
+	{"roomBg": "Demo4Map_3.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "north", "moveSpeed": "normal"},
+	{"roomBg": "Demo4Map_4.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "null", "moveSpeed": "stop"},
 	];
 
