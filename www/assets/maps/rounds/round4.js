2e63fddde8d2df41b8aa1e3a0fd40f853c3983f6
Maerus <maerus.sbkv@gmail.com>
2016-04-06 15:39:43 +0300

    fix master

--- www/assets/maps/rounds/round4.js
+++ www/assets/maps/rounds/round4.js
@@ -1,7 +1,7 @@
 'use strict';
 
 
-var round3 = [	//Walk around the wall grassland
+var round4 = [	//Walk around the wall grassland
 	{"roomBg": "Demo4Map-1.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "south", "moveSpeed": "normal"},
 	{"roomBg": "Demo4Map-2.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "east", "moveSpeed": "normal"},
 	{"roomBg": "Demo4Map-3.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "north", "moveSpeed": "normal"},
