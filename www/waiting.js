6dd5617328eedf13a42e2ffd38552102cfc0bdb1
Janika Kääriäinen <janika.kaariainen@helsinki.fi>
2016-04-13 15:43:41 +0300

    ...

--- www/waiting.js
+++ www/waiting.js
@@ -12,11 +12,19 @@ var cdText = 'Game starting in: ';
 var playerCount = Object.keys(game.playerList).length;
 var countDown;
 var lastDroppedCD = 0;
-
+var qr;
+var qrText;
 
 
 self.preload = function ()
 	{
+	qr = game.add.sprite(game.camera.x + game.camera.width, game.camera.y + game.camera.height, 'qr_niko'); //or: 'qr_janika'
+	qr.scale.x = 1.5*scalingFactors.x;
+	qr.scale.y = 1.5*scalingFactors.y;
+	qr.anchor.setTo(1,1);
+	qrText = game.add.text(0, 0, 'Join here ---> ', {font: '25px Courier', fill: '#ffffff'});
+	qrText.anchor.setTo(1,1);
+	qrText.position.setTo(qr.position.x - qr.width, qr.position.y);
 	waitingText = game.add.text(80, 150, baseWText, {font: '30px Courier', fill: '#ffffff'});
 	pText = game.add.text(80, 200, playersWaitingText + playerCount, {font: '30px Courier', fill: '#ffffff'});
 	countDownText = game.add.text(80, 250, '', {font: '30px Courier', fill: '#ffffff'});
@@ -26,6 +34,8 @@ self.preload = function ()
 
 self.update = function ()
 	{
+	qr.bringToTop();
+	qr.position.setTo(game.camera.x + game.camera.width, game.camera.y + game.camera.height);
 	playerCount = Object.keys(game.playerList).length;
 	if (playerCount < 1)
 		{
