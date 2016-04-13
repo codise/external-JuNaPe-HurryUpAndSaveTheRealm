4ba7a434f1b4b1000cc91cbe47dd87dd9e06e0bf
vtaipale <valtter.taipale@gmail.com>
2016-04-13 15:24:56 +0300

    Fixed enemy to wrong round

--- www/assets/maps/rounds/round5.js
+++ www/assets/maps/rounds/round5.js
@@ -8,4 +8,4 @@ round5.rooms = [
 	{"roomBg": "Demo5Map-3.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "east", "moveSpeed": "normal"},
 	{"roomBg": "Demo5Map-4.png", "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "null", "moveSpeed": "stop"},
 	];
-round4.boss = 'king';
+round5.boss = 'king';
