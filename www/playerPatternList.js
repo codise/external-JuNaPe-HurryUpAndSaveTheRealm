aa6b2eef04fbf99f7d75ec25626ab07ec0e11d68
Janika Kääriäinen <janika.kaariainen@helsinki.fi>
2016-04-22 15:37:05 +0300

    playerPatternList.js created

---
+++ www/playerPatternList.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var playerPatterns = {};
+
+//knigth
+playerPatterns["0"] =
+	{
+	baseMovementSpeed: 200
+
+
+	};
+
+//elf
+playerPatterns["1"] =
+	{
+
+
+
+	};
+
+//mage
+playerPatterns["2"] =
+	{
+
+
+
+	};
+
+//viking
+playerPatterns["3"] =
+	{
+
+
+
+	};
+
+//ninja
+playerPatterns["4"] =
+	{
+
+
+
+	};
+
+//warlock
+playerPatterns["5"] =
+	{
+
+
+
+	};
+
