baa8f410d4402f26aa022aadbfbf3d27799c2a24
vtaipale <valtter.taipale@gmail.com>
2016-04-25 14:56:26 +0300

    Even more tiles and first generic map
    More generic backgrounds (more detailed) and first new map.
    -Load.js filled as well

---
+++ www/assets/maps/rounds/generic1.js
@@ -0,0 +1,12 @@
+'use strict';
+
+	//Moving to west
+var generic1 = {};
+generic1.rooms = [
+	{"roomBg": "generic_Entrance_FromEast2"	, "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "west", "moveSpeed": "normal"},
+	{"roomBg": "generic_Turn_WtS1"			, "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "south", "moveSpeed": "normal"},
+	{"roomBg": "generic_Turn_EtN2"			, "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "west", "moveSpeed": "normal"},
+	{"roomBg": "generic_Straight_Vert3"		, "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "west", "moveSpeed": "normal"},
+	{"roomBg": "generic_DeadEnd_FromEast2"	, "roomJSON": "castlebasic.json", "tileset": "tileset.png", "moveDirection": "null", "moveSpeed": "stop"},
+	];
+generic1.boss = 'king';
