38dda95772519e1e9679a0d84e478382d6039d60
vtaipale <valtter.taipale@gmail.com>
2016-05-03 14:30:11 +0300

    Fixing texts

--- www/creditsBoard.js
+++ www/creditsBoard.js
@@ -22,10 +22,12 @@ var parseCredits = function ()
 	};
 
 		
+var creditInfo = [ 					{what: 'To ', who: ['OhtuProjekti-course, University of Helsinki CS, spring 2016']},
 									{what: 'Programming', who: ['Ahti Katiska', 'Niko Novitsky', 'Markus Sibakov', 'Janika Kääriäinen', 'Valtter Taipale']},
 									{what: 'Game Design', who: ['Valtter Taipale', 'Markus Sibakov', 'Niko Novinsky', 'Janika Kääriäinen']},
 									{what: 'Art', who: ['Valtter Taipale', 'Risto Ollila', 'Markus Sibakov']},
 									{what: 'Music', who: ['Kevin MacLeod (incompetech.com)']},
+									{what: 'Thanks', who: ['Matti Luukkainen', 'Leftpad','Our families', 'All the Cats of Earth']}
 									];
 
 var creditText = game.add.text(position.x, position.y, '', {font: '25px Courier', fill: '#ffffff'});
