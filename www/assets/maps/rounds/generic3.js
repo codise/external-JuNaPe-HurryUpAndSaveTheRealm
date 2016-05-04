51a3ce0380b637f95635ec32cfb0923e51bad580
Taipale Valtter H J <valtter.taipale@gmail.com>
2016-05-04 15:42:43 +0300

    Generic 3 map, demoes mid-round colliders

--- www/assets/maps/rounds/generic3.js
+++ www/assets/maps/rounds/generic3.js
@@ -11,7 +11,8 @@ room1colliders[1] = new wallCollider(1, maxTilesY, maxTilesX, 0, 1, 0);
 //bottom
 room1colliders[2] = new wallCollider(maxTilesX, 0.9, 0, maxTilesY, 0, 1);
 
-
+room1colliders.p1 = new smallCollisionSprite( 4, 5, 'pillarGone');
+room1colliders.p2 = new smallCollisionSprite( 4, 10, 'pillar');
 
 
 var room2colliders = {};
@@ -37,7 +38,7 @@ room3colliders[1] = new wallCollider(maxTilesX, 0.9, 0, maxTilesY, 0, 1);
 room3colliders[2] = new wallCollider(10, 1.9, 0, 7.1, 0, 0);
 
 //middle Wall right
-room3colliders[2] = new wallCollider(10, 1.9, 14, 7.1, 0, 0);
+room3colliders[3] = new wallCollider(10, 1.9, 14, 7.1, 0, 0);
 
 
 
@@ -47,7 +48,8 @@ room4colliders[0] = new wallCollider(maxTilesX, 2, 0, 0, 0, 0);
 //bottom
 room4colliders[1] = new wallCollider(maxTilesX, 0.9, 0, maxTilesY, 0, 1);
 
-
+room4colliders.p1 = new smallCollisionSprite( 4, 5, 'pillar');
+room4colliders.p2 = new smallCollisionSprite( 4, 10, 'pillar');
 
 var room5colliders = {};
 //top
@@ -57,20 +59,20 @@ room5colliders[1] = new wallCollider(maxTilesX, 0.9, 0, maxTilesY, 0, 1);
 //left
 room5colliders[2] = new wallCollider(0.75, maxTilesY, 0, 0, 0, 0);
 //pillars
-room5colliders.p1 = new smallCollisionSprite( 4, 5, 'pillar');
+room5colliders.p1 = new smallCollisionSprite( 4, 5, 'pillarGone');
 room5colliders.p2 = new smallCollisionSprite( 19, 5, 'pillar');
 room5colliders.p3 = new smallCollisionSprite( 4, 10, 'pillar');
 room5colliders.p4 = new smallCollisionSprite( 19, 10, 'pillar');
 
 
 
-generic1.rooms = [
+generic3.rooms = [
 	{"roomBg": "generic_Entrance_FromEast3", "colliders": room1colliders, "moveDirection": "west", "moveSpeed": "normal"},
 	{"roomBg": "generic_Straight_Vert5"	, "colliders": room2colliders, "moveDirection": "west", "moveSpeed": "normal"},
 	{"roomBg": "generic_Straight_Vert6"	, "colliders": room3colliders, "moveDirection": "west", "moveSpeed": "normal"},
-	{"roomBg": "generic_Straight_Vert4"	, "colliders": room4colliders, "moveDirection": "west", "moveSpeed": "fast"},
+	{"roomBg": "generic_Straight_Vert4"	, "colliders": room4colliders, "moveDirection": "west", "moveSpeed": "normal"},
 	{"roomBg": "generic_DeadEnd_FromEast1", "colliders": room5colliders, "moveDirection": "null", "moveSpeed": "stop"},
 	];
 
-generic1.boss = 'king';
-round2.bgm = 'bgm01'
+generic3.boss = 'tentacle';
+generic3.bgm = 'bgm01'
--- www/util.js
+++ www/util.js
@@ -34,7 +34,7 @@ self.x = xPos * tileWidth;
 self.y = yPos * tileHeight;
 self.anchorX = xAnchor;
 self.anchorY = yAnchor;
-self.image = 'test';
+self.image = 'empty';
 /*
 self.image = 'test';	//to test wall collision
 self.image = 'empty';	//to testimages to remain invisible
